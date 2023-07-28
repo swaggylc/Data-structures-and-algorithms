c330966db4ae2f6233229c64e01f42da80b94ff9
lucan <lucanlc0417@gmail.com>
2023-07-28 10:19:20 +0800

    toString和insert方法实现

--- 4.链表的封装.js
+++ 4.链表的封装.js
@@ -6,10 +6,10 @@ function LinkedList() {
         this.next = null
     }
 
-
     // 属性
     this.head = null
     this.length = 0
+
     // 方法
     // 向链表尾部添加一个元素
     LinkedList.prototype.append = function (data) {
@@ -29,13 +29,56 @@ function LinkedList() {
             // 指向null则将该节点的next指向node
             current.next = node
         }
-
-        this.length++
+        this.length += 1
+    }
+    // toString方法
+    LinkedList.prototype.toString = function () {
+        let LinkedListString = ''
+        let current = this.head
+        while (current) {
+            LinkedListString += current.data + ' '
+            current = current.next
+        }
+        return LinkedListString
+    }
+    // insert方法
+    LinkedList.prototype.insert = function (data, position) {
+        // 判断position是否越界
+        if (position < 0 || position > this.length) return false
+        // 创建节点
+        let node = new Node(data)
+        // 如果插入的是第0个位置
+        if (position == 0) {
+            node.next = this.head
+            this.head = node
+        } else {
+            // 非第0个位置
+            let current = this.head
+            let previous = null
+            let index = 0
+            while (index++ < position) {
+                previous = current
+                current = current.next
+            }
+            node.next = current
+            previous.next = node
+        }
+        this.length += 1
+        return true
     }
 
 
 
 
-
-
 }
+
+// 使用
+let link = new LinkedList()
+link.append(1)
+link.append(2)
+link.append(3)
+link.append(4)
+link.append(5)
+
+console.log(link);
+console.log(link.toString());
