1b08761f8caa6e97892f488862b1aae91d83e55b
lucan <lucanlc0417@gmail.com>
2023-08-04 11:22:47 +0800

    封装二叉搜索树

---
+++ 11.封装二叉搜索树.js
@@ -0,0 +1,33 @@
+// 封装二叉搜索树
+function BinarySerachTree() {
+    // 节点类
+    function Node(key) {
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+
+
+    // 属性
+    this.root = null
+
+    // 方法
+    // insert(key) 插入新的节点
+
+    // search(key) 查找节点
+
+    // reValue(key,value) 修改操作，暂不考虑
+
+    // inOrderTraverse 中序遍历
+
+    // preOrderTraverse 先序遍历
+
+    // postOrderTraverse 后序遍历
+
+    // min 树中的最小值
+
+    // max 树中的最大值
+
+    // remove(key) 移除某个节点
+
+}
